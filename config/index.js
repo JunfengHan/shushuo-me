df03785f584c415ddf67abf72183a5798e3dc034
hanjunfeng <hanjunfeng@hanjunfeng>
2019-01-09 16:01:26 +0800

    Update: 配置dev环境启动

--- config/index.js
+++ config/index.js
@@ -5,6 +5,12 @@ module.exports = {
         env: {
             NODE_ENV: "production"
         },
-        index: path.resolve(__dirname, '../')
+        index: path.resolve(__dirname, '../shushuo/index.html')
+    },
+    dev: {
+        env: {
+            NODE_ENV: "development"
+        },
+        port: 4000
     }
 }
